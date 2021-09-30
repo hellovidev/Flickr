d89412618c98875726cc626191e466eaaebab004
Hello VI Dev <79969749+hellovidev@users.noreply.github.com>
2021-09-30 21:56:30 +0300

    Change style of menu item

--- Flickr/App Modules/Gallery Module/GalleryViewController.swift
+++ Flickr/App Modules/Gallery Module/GalleryViewController.swift
@@ -17,17 +17,17 @@ class GalleryViewController: UIViewController, UINavigationControllerDelegate {
     private let collectionCellReuseIdentifier: String = "CollectionReusableCell"
     
     var viewModel: GalleryViewModel!
-
+    
     var refresher:UIRefreshControl!
-
+    
     @objc
     func loadData() {
         refresher.beginRefreshing()
-       //code to execute during refresher
-
+        //code to execute during refresher
+        
         viewModel.gallery = []
         collectionView.reloadData()
-    
+        
         
         viewModel.requestPhotoLinkInfoArray { [weak self] result in
             switch result {
@@ -39,9 +39,9 @@ class GalleryViewController: UIViewController, UINavigationControllerDelegate {
             }
         }
         
-     }
+    }
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -67,7 +67,7 @@ class GalleryViewController: UIViewController, UINavigationControllerDelegate {
         NotificationCenter.default.addObserver(self, selector: #selector(imageUploadNotification), name: Notification.Name("ImageUpload"), object: nil)
         
         
-
+        
     }
     
     
@@ -81,13 +81,13 @@ class GalleryViewController: UIViewController, UINavigationControllerDelegate {
         viewModel.networkService.uploadProgress = 0
     }
     
-
-
+    
+    
     
     deinit {
         print("\(type(of: self)) deinited.")
     }
-
+    
 }
 
 // MARK: - UICollectionViewDataSource
@@ -105,10 +105,13 @@ extension GalleryViewController: UICollectionViewDataSource {
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: collectionCellReuseIdentifier, for: indexPath)
         
+        let lpgr = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
+        cell.addGestureRecognizer(lpgr)
+        
         for subview in cell.subviews {
-             // you can place "if" condition to remove image view, labels, etc.
-             //it will remove subviews of cell's content view
-             subview.removeFromSuperview()
+            // you can place "if" condition to remove image view, labels, etc.
+            //it will remove subviews of cell's content view
+            subview.removeFromSuperview()
         }
         cell.backgroundView = nil
         cell.backgroundColor = nil
@@ -119,17 +122,24 @@ extension GalleryViewController: UICollectionViewDataSource {
             newPhotoView.frame = cell.bounds
             cell.addSubview(newPhotoView)
         } else {
+            
+            
+            cell.isUserInteractionEnabled = true
+            
+            let interaction = UIContextMenuInteraction(delegate: self)
+            cell.addInteraction(interaction)
+            
             if viewModel.numberOfItems != .zero {
-            self.viewModel.requsetPhoto(index: indexPath.row - 1) { result in
-                switch result {
-                case .success(let image):
-                    let imageView: UIImageView = .init(image: image)
-                    imageView.frame = cell.bounds
-                    cell.backgroundView = imageView
-                case .failure(let error):
-                    print(error)
+                self.viewModel.requsetPhoto(index: indexPath.row - 1) { result in
+                    switch result {
+                    case .success(let image):
+                        let imageView: UIImageView = .init(image: image)
+                        imageView.frame = cell.bounds
+                        cell.backgroundView = imageView
+                    case .failure(let error):
+                        print(error)
+                    }
                 }
-            }
             }
             cell.backgroundColor = .orange
         }
@@ -137,7 +147,25 @@ extension GalleryViewController: UICollectionViewDataSource {
         return cell
     }
     
-
+    @objc func handleLongPress(gesture : UILongPressGestureRecognizer!) {
+        if gesture.state != .ended {
+            return
+        }
+        
+        let p = gesture.location(in: self.collectionView)
+        
+        if let indexPath = self.collectionView.indexPathForItem(at: p) {
+            // get the cell at indexPath (the one you long pressed)
+            let cell = self.collectionView.cellForItem(at: indexPath)
+            
+            // collectionView.deleteItems(at: [indexPath])
+            // do stuff with the cell
+        } else {
+            print("couldn't find index path")
+        }
+    }
+    
+    
     
     @objc
     func onTapAddNewPhoto(_ sender: UIButton) {
@@ -157,11 +185,13 @@ extension GalleryViewController: UICollectionViewDataSource {
             imagePicker.sourceType = .photoLibrary
             
             present(imagePicker, animated: true, completion: nil)
-
+            
         }
     }
     
 }
+
+
 
 extension GalleryViewController: PHPickerViewControllerDelegate {
     
@@ -176,15 +206,15 @@ extension GalleryViewController: PHPickerViewControllerDelegate {
                 guard let data = data else { return }
                 
                 self?.viewModel.networkService.uploadNewPhoto(data, title: "New poster", description: "Added photo from iOS application.") { result in
-                                    switch result {
-                                    case .success(_):
-                                        NotificationCenter.default.post(name: Notification.Name("ImageUpload"), object: nil)
-
-                                        break
-                                    case .failure(let error):
-                                        print(error)
-                                    }
-                                }
+                    switch result {
+                    case .success(_):
+                        NotificationCenter.default.post(name: Notification.Name("ImageUpload"), object: nil)
+                        
+                        break
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
             })
         }
         
@@ -200,39 +230,40 @@ extension GalleryViewController: UIImagePickerControllerDelegate {
         print("Cancel")
         dismiss(animated: true, completion: nil)
     }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let selectedImage = info[.originalImage] as? UIImage else {
             return
         }
         
         if let data = selectedImage.pngData() {
-        
-        viewModel.networkService.uploadNewPhoto(data, title: "New poster", description: "Added photo from iOS application.") { result in
-                            switch result {
-                            case .success(_):
-                                NotificationCenter.default.post(name: Notification.Name("ImageUpload"), object: nil)
-
-                                break
-                            case .failure(let error):
-                                print(error)
-                            }
-                        }
+            
+            viewModel.networkService.uploadNewPhoto(data, title: "New poster", description: "Added photo from iOS application.") { result in
+                switch result {
+                case .success(_):
+                    NotificationCenter.default.post(name: Notification.Name("ImageUpload"), object: nil)
+                    
+                    break
+                case .failure(let error):
+                    print(error)
+                }
+            }
         }
         dismiss(animated: true, completion: nil)
-//        guard let image = info[.editedImage] as? UIImage else { return }
-//
-//        let imageName = UUID().uuidString
-//        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
-//
-//        if let jpegData = image.jpegData(compressionQuality: 0.8) {
-//            try? jpegData.write(to: imagePath)
-//        }
-//
-//        dismiss(animated: true)
+        //        guard let image = info[.editedImage] as? UIImage else { return }
+        //
+        //        let imageName = UUID().uuidString
+        //        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        //
+        //        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+        //            try? jpegData.write(to: imagePath)
+        //        }
+        //
+        //        dismiss(animated: true)
         
-//        guard let image = info[.editedImage] as? UIImage else {
-//            return self.pickerController(picker, didSelect: nil)
-//        }
+        //        guard let image = info[.editedImage] as? UIImage else {
+        //            return self.pickerController(picker, didSelect: nil)
+        //        }
     }
     
 }
@@ -263,3 +294,131 @@ extension GalleryViewController: UICollectionViewDelegate {
     }
     
 }
+
+extension GalleryViewController: UIContextMenuInteractionDelegate {
+    
+    func contextMenuInteraction(_ interaction: UIContextMenuInteraction, configurationForMenuAtLocation location: CGPoint) -> UIContextMenuConfiguration? {
+        let configuration = UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { actions -> UIMenu? in
+            
+            let removeIcon = UIImage(systemName: "trash")//?.withTintColor(.red, renderingMode: .alwaysOriginal)
+            
+            let removeItem = UIAction(title: "Delete", image: removeIcon, attributes: .destructive) { action in
+                let item = interaction.location(in: self.collectionView)
+                
+                if let indexPath = self.collectionView.indexPathForItem(at: item) {
+                    // get the cell at indexPath (the one you long pressed)
+                    //let cell = self.collectionView.cellForItem(at: indexPath)
+                    
+                    self.viewModel.gallery.remove(at: indexPath.row - 1)
+                    self.collectionView.deleteItems(at: [indexPath])
+                    // do stuff with the cell
+                } else {
+                    print("couldn't find index path")
+                }
+                print("Remove User action was tapped")
+            }
+            let menu = UIMenu(title: "", options: .displayInline, children: [removeItem])
+            return menu
+        }
+        return configuration
+    }
+    
+}
+
+/*
+ 
+ class Some {
+ 
+ @objc func handleLongPress(gesture : UILongPressGestureRecognizer!) {
+ if gesture.state != .ended {
+ return
+ }
+ 
+ let p = gesture.location(in: self.collectionView)
+ 
+ if let indexPath = self.collectionView.indexPathForItem(at: p) {
+ // get the cell at indexPath (the one you long pressed)
+ let cell = self.collectionView.cellForItem(at: indexPath)
+ // do stuff with the cell
+ } else {
+ print("couldn't find index path")
+ }
+ }
+ }
+ 
+ let some = Some()
+ let lpgr = UILongPressGestureRecognizer(target: some, action: #selector(Some.handleLongPress))
+ 
+ 
+ 
+ 
+ -(void) handleLongPress:(UILongPressGestureRecognizer *)sender
+ {
+ if (sender.state == UIGestureRecognizerStateBegan)
+ {
+ //Start a timer and perform action after whatever time interval you want.
+ }
+ if (sender.state == UIGestureRecognizerStateEnded)
+ {
+ //Check the duration and if it is less than what you wanted, invalidate the timer.
+ }
+ }
+ 
+ // DELETE
+ 
+ 
+ 
+ @IBAction func deleteItem(_ sender: Any) {
+ if let selectedCells = collectionView.indexPathsForSelectedItems {
+ // 1
+ let items = selectedCells.map { $0.item }.sorted().reversed()
+ // 2
+ for item in items {
+ modelData.remove(at: item)
+ }
+ // 3
+ collectionView.deleteItems(at: selectedCells)
+ deleteButton.isEnabled = false
+ }
+ }
+ 
+ 
+ // POP UP
+ 
+ let usersItem = UIAction(title: "Users", image: UIImage(systemName: "person.fill")) { (action) in
+ 
+ print("Users action was tapped")
+ }
+ 
+ let addUserItem = UIAction(title: "Add User", image: UIImage(systemName: "person.badge.plus")) { (action) in
+ 
+ print("Add User action was tapped")
+ }
+ 
+ let removeUserItem = UIAction(title: "Remove User", image: UIImage(systemName: "person.fill.xmark.rtl")) { (action) in
+ print("Remove User action was tapped")
+ }
+ 
+ let menu = UIMenu(title: "My Menu", options: .displayInline, children: [usersItem , addUserItem , removeUserItem])
+ 
+ 
+ 
+ 
+ 
+ showButton.menu = menu
+ showButton.showsMenuAsPrimaryAction = true
+ 
+ 
+ 
+ let navItems = [UIBarButtonItem(image:  UIImage(systemName: "plus"), primaryAction: plusAction, menu: menu) ,
+ .fixedSpace(10),
+ UIBarButtonItem(systemItem: .search , menu: menu)]
+ 
+ let plusAction = UIAction(title: "plusAction"){ (action) in
+ print("Plus Action action was tapped ")
+ }
+ 
+ self.navigationItem.leftBarButtonItems = navItems
+ 
+ 
+ */
